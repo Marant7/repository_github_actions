d60b95c8c50a8b27a4b4a0507bada5e048be6d02
marant7 <mf2018000597@virtual.upt.pe>
2024-12-08 02:01:58 -0500

    app2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
     env: {
-      commonjs: true,
-      es2021: true,
       node: true,
       jest: true,
+      es2021: true
     },
     extends: 'airbnb-base',
     parserOptions: {
-      ecmaVersion: 12,
+      ecmaVersion: 'latest'
     },
     rules: {
       'class-methods-use-this': 'off',
-      'no-console': 'off',
-    },
+      'no-console': 'off'
+    }
   };
